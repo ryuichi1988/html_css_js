338b61e4c3b60411586ade54c97734448feb5448
ryuichi1988 <66043207+ryuichi1988@users.noreply.github.com>
2020-08-20 18:43:55 +0900

    初学html和css 2020年8月20日

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// JavaScript Document
+<!doctype html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+<meta charset="utf-8">
+<title>MyTestTitle</title>
+<link rel="MyFirstHtml" href="file:///D|/html_ccs_jsUsers/MyFirstHtml.css">
+<style>
+	
+</style>
+
+	
+</head>
+
+<body class=body>
+<p>111TTTSS我的测试按钮哦</p>
+	<h1>"这是h1标签撒"</h1>
+	<div class=cssin>嵌入式样式</div>
+	<p class=text>嵌入方式的 CSS 只对当前的网页有效。因为 CSS 代码是在 HTML 文件中，所以会使得代码比较集中，当我们写模板网页时这通常比较有利。因为查看模板代码的人可以一目了然地查看 HTML 结构和 CSS 样式。因为嵌入的 CSS 只对当前页面有效，所以当多个页面需要引入相同的 CSS 代码时，这样写会导致代码冗余，也不利于维护。
+
+</p>
+	<h2 class=text>this is test for impact</h2>
+	<a  href="file:///C|/Users/Jinzai01/Documents/baidu.com" class="ytest">这是a标签</a><p>
+	
+	<img alt="1" src="https://prog-8.com/images/html/beginner/wanko.jpg"></p>
+	<img src="https://prog-8.com/images/html/beginner/school.jpg">
+	<oi>
+		<li>test1</li>
+		<li>test2</li>
+	</oi>
+	<h4 class=h4tag>this is h4 tag</h4>
+	
+</body>
+</html>
